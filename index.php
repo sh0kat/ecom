c5acbf3cae349c1548372d5579b894632d1c942c
Shokat <shokatsaifi@gmail.com>
2014-04-08 21:03:51 +0530

    Index

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
-
-echo "Hello" ;
+session_start();
+if ((isset($_SESSION['user_logged']) &&
+$_SESSION['user_logged'] != "") ||
+(isset($_SESSION['user_password']) && $_SESSION['user_password'] != "")) {
+include "logged_user.php";
+} else {
+include "unlogged_user.php";
+}
 ?>
